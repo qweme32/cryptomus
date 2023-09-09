d4457a4f7d62aef3407551f9ce89614c0d7ee7f7
Qweme Dev <99718350+qweme32@users.noreply.github.com>
2023-09-10 00:29:49 +0300

    add

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-import { E_HTTP, E_TOKEN, I_CREATE_PAYMENT_REQUEST, I_CREATE_PAYMENT_RESPONSE } from './types';
+import { E_HTTP, E_TOKEN, I_CREATE_PAYMENT_REQUEST, I_CREATE_PAYMENT_RESPONSE, I_CREATE_STATIC_WALLET_REQUEST, I_CREATE_STATIC_WALLET_RESPONSE, I_GEN_QR_REQUEST, I_GEN_QR_RESPONSE } from './types';
 export declare class Cryptomus {
     private readonly merchant;
     private readonly paymentToken;
@@ -21,6 +21,20 @@ export declare class Cryptomus {
      * @return {Promise<I_CREATE_PAYMENT_RESPONSE>} The response of creating a payment.
      */
     createPayment(options: I_CREATE_PAYMENT_REQUEST): Promise<I_CREATE_PAYMENT_RESPONSE>;
+    /**
+     * Creates a static wallet.
+     *
+     * @param {I_CREATE_STATIC_WALLET_REQUEST} options - The options for creating the static wallet.
+     * @return {Promise<I_CREATE_STATIC_WALLET_RESPONSE>} A promise that resolves with the response of creating the static wallet.
+     */
+    createStaticWallet(options: I_CREATE_STATIC_WALLET_REQUEST): Promise<I_CREATE_STATIC_WALLET_RESPONSE>;
+    /**
+     * Generates a QR code based on the provided options.
+     *
+     * @param {I_GEN_QR_REQUEST} options - The options for generating the QR code.
+     * @return {Promise<I_GEN_QR_RESPONSE>} - A promise that resolves to the generated QR code response.
+     */
+    genQr(options: I_GEN_QR_REQUEST): Promise<I_GEN_QR_RESPONSE>;
     /**
      * Generates a signature for the given data and key.
      *
